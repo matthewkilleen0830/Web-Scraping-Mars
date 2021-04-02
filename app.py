aef236bad587d0ca6d271a5e169f0f9cd273e509
matthewkilleen0830 <matthew.killeen0830@gmail.com>
2021-04-02 13:31:06 -0500

    All code seems complete but cannot connect to MongoDB to store scraped data.

--- app.py
+++ app.py
@@ -1,32 +1,37 @@
 # Dependencies and setup
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scrape_mars
 
 # Create instance of Flask app
 app = Flask(__name__)
 
-# Initialize PyMongo to work with MongoDBs (this needs work with tutor)
-mongo = PyMongo(app, uri = "mongodb://localhost:27017/marsApp")
-marsData = mongo.db.marsData
+# Initialize PyMongo to work with MongoDBs (something is malfunctioning in this block - my database isn't being populated with scraped data)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/marsData"
+mongo = PyMongo(app)
+# mongo = PyMongo(app, uri = "mongodb://localhost:27017/marsData")
+# marsData = mongo.db.marsData
 
 # Create route that renders index.html template
 @app.route("/")
 def index():
-    return render_template("index.html")
+    collection = mongo.db.collection.find_one()
+    return render_template("index.html", collection = collection)
 
 # Create a route called /scrape that will import your scrape_mars.py script and call your scrape function
 @app.route("/scrape")
 def scrape():
 
+    collection = mongo.db.collection
+
     # Call scrape function
     scrapedData = scrape_mars.scrape()
 
     # Update MongoDB with scraped data
-    marsData.update({}, scrapedData, upsert = True)
+    collection.update({}, scrapedData, upsert = True)
 
     # Redirect to landing page
-    return redirect("/")
+    return redirect("/", code = 302)
 
 # Define main behavior
 if __name__ == "__main__":
--- scrape_mars.py
+++ scrape_mars.py
@@ -5,52 +5,38 @@ from flask import Flask, render_template
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 import os
-import requests
-import pymongo
+import time
 
 # Define scrape function
 def scrape():
 
-    # Initialize browser/set up Splinter (this needs work with tutor)
-    executable_path = {"executable_path": "/Users/matth/.wdm/drivers/chromedriver/win32/89.0.4389.23"}
-    # executable_path = {"executable_path":ChromeDriverManager().install()}
+    # Initialize browser/set up Splinter
+    executable_path = {"executable_path":ChromeDriverManager().install()}
     browser = Browser("chrome", **executable_path, headless = False)
 
-    # NASA Mars News -----------------------------------------------------------------
+    # NASA MARS NEWS -----------------------------------------------------------------
 
     # URL of page to be scraped
     newsURL = "https://redplanetscience.com"
     browser.visit(newsURL)
+    time.sleep(1)
+    
+    # HTML object
+    # html = browser.html
+    
+    # Parse HTML with BeautifulSoup
+    # soup = bs(html, "html.parser")
+    
+    # Declare variables to store scraped Title and Teaser 
+    newsTitle = browser.find_by_css(".content_title")[0].text
+    paragraphText = browser.find_by_css(".article_teaser_body")[0].text
 
-    # Loop through homepage (this works from class example but maybe only one loop is needed?  ask tutor)
-    for x in range(1):
-    
-        # HTML object
-        html = browser.html
-    
-        # Parse HTML with BeautifulSoup
-        soup = bs(html, "html.parser")
-    
-        # Retrieve all elements that contain News Titles and Paragraph Text
-        articles = soup.find_all("div", class_ = "list_text")
-    
-        # Iterate through each headline
-        for article in articles:
-        
-            # Use BeautifulSoup's find() method to navigate and retrieve attributes
-            newsTitle = article.find("div", class_ = "content_title").text
-            paragraphText = article.find("div", class_ = "article_teaser_body").text
-            print("------------------------------------------")
-            print(f"Article:  {newsTitle}")
-            print(f"Teaser:  {paragraphText}")
-            print(" ")
-            print(f"Mars News Scraping Complete.")
-    
-    # JPL Mars Space Images - Featured Image ------------------------------------------
+    # JPL MARS SPACE IMAGES - FEATURED IMAGE ------------------------------------------
 
     # URL of page to be scraped
     imageURL = "https://spaceimages-mars.com/"
     browser.visit(imageURL)
+    time.sleep(1)
 
     # HTML object
     imageHTML = browser.html
@@ -62,92 +48,81 @@ def scrape():
     featuredImage = imageSoup.find("a", class_ = "showimg fancybox-thumbs")["href"]
 
     # Declare variable and combine main URL with link
-    featured_image_url = f"https://spaceimages-mars.com/{featuredImage}"
-    print(f"Featured Image URL:  {featured_image_url}")
-    print(" ")
-    print(f"JPL Mars Space Images - Featured Image Scraping Complete.")
+    featured_image_url = (f"https://spaceimages-mars.com/{featuredImage}")
 
-    # Mars Facts ----------------------------------------------------------------------
+    # MARS FACTS ----------------------------------------------------------------------
 
     # Use the read_html function in pandas to automatically scrape tabular data
     tableURL = "https://galaxyfacts-mars.com"
-    tables = pd.read_html(tableURL)
+    marsTable = pd.read_html(tableURL)
 
     # Slice off DataFrame that we want using normal indexing
-    marsFacts_df = tables[0]
-
-    # Export DataFrame to HTML
-    marsFacts_df.to_html("Resources/DataFrame.html", index = False)
+    marsFacts_df = marsTable[0]
 
     # Extract first row from DataFrame and set it as the header
     new_header = marsFacts_df.iloc[0]
     marsFacts_df = marsFacts_df[1:]
     marsFacts_df.columns = new_header
 
-    print(f" ")
-    print(f"Mars Facts Scraping Complete.")
+    # Export DataFrame to HTML
+    marsFacts_table = marsFacts_df.to_html()
 
-    # Mars Hemispheres ----------------------------------------------------------------
+    # MARS HEMISPHERES ----------------------------------------------------------------
 
     # URL of page to be scraped
     hemispheresURL = "https://marshemispheres.com/"
     browser.visit(hemispheresURL)
+    time.sleep(1)
 
     # HTML object
-    hemispheresHTML = browser.html
+    # hemispheresHTML = browser.html
     
     # Parse HTML with BeautifulSoup
-    hemispheresSoup = bs(hemispheresHTML, "html.parser")
+    # hemispheresSoup = bs(hemispheresHTML, "html.parser")
     
     # Retrieve all elements that contain image URLs to the full resolution image
-    links = hemispheresSoup.find_all("div", class_ = "description")
+    # links = hemispheresSoup.find_all("div", class_ = "")
 
-    # Create empty list to be appended with dictionaries
+    # Create empty list to be appended with dictionary
     hemisphere_image_urls = []
 
-    # Iterate through each description
-    for link in links:
-        
-        # Use BeautifulSoup's find() method to navigate and retrieve attributes
-        imageTitle = link.find("h3").text
-        imageLink = link.find("a", class_ = "itemLink product-item")["href"]
-    
-        # Visit the link with the full resolution image
-        browser.visit(hemispheresURL + imageLink)
-    
-        # HTML object
-        linkHTML = browser.html
-    
-        # Parse HTML with BeautifulSoup
-        linkSoup = bs(linkHTML, "html.parser")
-    
-        # Full resolution image URL
-        fullURL = hemispheresURL + linkSoup.find("img", class_ = "wide-image")["src"]
-    
-        # Append to list of dictionaries
-        hemisphere_image_urls.append({"title":imageTitle, "img_url":fullURL})
-    
-        # Display titles and links
-        print("------------------------------------------")
-        print(f"Title:  {imageTitle}")
-        print(f"Link:   {fullURL}")
+    # Declare variable to store scraped links
+    imageLinks = browser.find_by_css("a.itemLink img")
 
-    # Display end statement    
-    print(" ")
-    print("Mars Hemispheres Scraping Complete.")
+    # Iterate through each element
+    for link in range(len(imageLinks)):
+        
+        # Create empty dictionary
+        imageDict = {}
+
+        # Locate and follow each subsequent link
+        browser.find_by_css("a.product-item img")[link].click()
+
+        # Declare variable to store scraped image URL
+        image = browser.links.find_by_text("Sample").first
+
+        # Append scraped image URL and scraped Title to dictionary
+        imageDict["img_url"] = image["href"]
+        imageDict["title"] = browser.find_by_css("h2.title").text
+        
+        # Append list with scraped dictionary
+        hemisphere_image_urls.append(imageDict)
+        
+        # Go back
+        browser.back()
+
+    # ALL PAGES DATA ------------------------------------------------------------------
+
+    # Create and append single dictionary with all scraped data
+    scrapedData = {
+    "News_Title":newsTitle, 
+    "News_Text":paragraphText,
+    "Featured_Image":featured_image_url,
+    "Mars_Facts":marsFacts_table,
+    "Hemisphere_Images":hemisphere_image_urls
+    }
 
     # Close remote browser
     browser.quit()
 
-    # All Pages Data ------------------------------------------------------------------
-
-    # Return one Python dictionary containing all of the scraped data
-    scrapedData = {
-        "News_Title":newsTitle, "News_Text":paragraphText,
-        "Featured_Image":featured_image_url,
-        "Mars_Facts":marsFacts_table,
-        "Hemisphere_Images":hemisphere_image_urls
-    }
-
-    # Return dictionary
     return scrapedData
